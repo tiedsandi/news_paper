b07bbf2e74dfec1aa41310f71b79142d9b488cf9
tiedsandi <fachransandi@gmail.com>
2024-10-07 10:35:49 +0700

    add image page

---
+++ app/news/[slug]/image/page.js
@@ -0,0 +1,15 @@
+import {DUMMY_NEWS} from '@/dummy-news';
+
+export default function ImagePage({params}) {
+  const newItemSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newItemSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
+  return (
+    <div className='fullscreen-image'>
+      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+    </div>
+  );
+}
