66f9f96f53e75fca2e43941e5b21caabf22fc2c8
tiedsandi <fachransandi@gmail.com>
2024-10-07 11:12:01 +0700

    fixing bug Defining the Base HTML Document

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,20 +1,15 @@
 import MainHeader from '@/components/main-header';
-import '../globals.css';
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 };
 
-export default function RootLayout({children}) {
+export default function ContentLayout({children}) {
   return (
-    <html lang='en'>
-      <body>
-        <div id='page'>
-          <MainHeader />
-          {children}
-        </div>
-      </body>
-    </html>
+    <div id='page'>
+      <MainHeader />
+      {children}
+    </div>
   );
 }
