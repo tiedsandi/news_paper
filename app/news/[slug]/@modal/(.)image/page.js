91081833e7fc56801bf4f362b00fb6d0365ba208
tiedsandi <fachransandi@gmail.com>
2024-10-07 11:01:47 +0700

    add userRouter for close the modal

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,16 +1,21 @@
+'use client';
+
 import {DUMMY_NEWS} from '@/dummy-news';
-import {notFound} from 'next/navigation';
+import {notFound, useRouter} from 'next/navigation';
 
 export default function InterceptedImagePage({params}) {
+  const router = useRouter();
+
   const newItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newItemSlug);
 
   if (!newsItem) {
     notFound();
   }
+
   return (
     <>
-      <div className='modal-backdrop' />
+      <div className='modal-backdrop' onClick={router.back} />
       <dialog className='modal' open>
         <div className='fullscreen-image'>
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
