9a2844d93a60c19f861aebc2866e263e33b98bea
tiedsandi <fachransandi@gmail.com>
2024-10-07 11:24:36 +0700

    add middleware and fixing defining the base HTML Document

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,14 +1,8 @@
-import '../globals.css';
-
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 };
 
-export default function RootLayout({children}) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  );
+export default function MarketingLayout({children}) {
+  return <main>{children}</main>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,14 @@
+import './globals.css';
+
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 };
 
-export default function MarketingLayout({children}) {
-  return <main>{children}</main>;
+export default function RootLayout({children}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  );
 }
